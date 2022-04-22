85bf92048b095b003c9dfcc526c41326dbb1ad91
niqeauxlaayy <77495054+niqeauxvoux@users.noreply.github.com>
2022-04-22 18:29:49 +0200

    Initial Commit

---
+++ GoTHouses/GoTHousesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTHousesApp.swift
+//  GoTHouses
+//
+//  Created by MacBook Pro on 22.04.22.
+//
+
+import SwiftUI
+
+@main
+struct GoTHousesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
